b075a907a5f5b9c9457b4798fb254494e84939fb
jiangyajun <jiangyajun@xunyou.com>
2025-07-21 11:29:26 +0800

    修改 GetPortsNumber 函数以使用 rte_eth_dev_count 代替 my_rte_eth_dev_count

--- internal/low/low.go
+++ internal/low/low.go
@@ -598,7 +598,7 @@ func FreeKNI(port uint16) error {
 
 // GetPortsNumber gets total number of available Ethernet devices.
 func GetPortsNumber() int {
-	return int(C.my_rte_eth_dev_count())
+	return int(C.rte_eth_dev_count())
 }
 
 func CheckPortRSS(port uint16) int32 {
